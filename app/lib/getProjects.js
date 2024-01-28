ba724227943815e9143390f4f6af3d67da64f1e1
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-01-28 20:29:27 +0600

    base url function added

--- app/lib/getProjects.js
+++ app/lib/getProjects.js
@@ -1,6 +1,13 @@
+export function getBaseUrl() {
+  if (typeof window !== 'undefined') return '';
+  const vc = process.env.VERCEL_URL;
+  if (vc) return `https://${vc}`;
+  return 'http://localhost:3000';
+}
+
 export default async function getProjects() {
   // const res = await fetch(`${process.env.HOST_URL}/api/projects`);
-  const res = await fetch(`https://ah-akash.vercel.app/api/projects`);
+  const res = await fetch(`${getBaseUrl()}/api/projects`);
   console.log(res);
   if (!res.ok) {
     throw new Error('Failed to fetch projects');
