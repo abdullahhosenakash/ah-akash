172008fdef2b445eadd32dc5ddaa09e5bb93a396
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-01-28 17:10:20 +0600

    api bug fixed

--- app/lib/getProject.js
+++ app/lib/getProject.js
@@ -1,5 +1,5 @@
 export default async function getProjects(_id) {
-  const res = await fetch(`${process.env.HOST}/api/projects/${_id}`);
+  const res = await fetch(`${process.env.HOST_URL}/api/projects/${_id}`);
   if (!res.ok) {
     throw new Error('Failed to fetch projects');
   }
