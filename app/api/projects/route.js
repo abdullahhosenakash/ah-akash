8936a677dddd021a3d590e79976a53b811b12f7c
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-01-28 19:30:59 +0600

    mongodb configuration modified

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,7 +1,7 @@
 import projectsCollection from '@/app/utils/mongodb';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request) {
   const projects = await projectsCollection.find({}).toArray();
   return NextResponse.json(projects);
 }
