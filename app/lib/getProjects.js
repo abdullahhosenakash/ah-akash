dbc3f7fde17abb815b041554f05fc2cbc812558e
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-01-28 19:41:14 +0600

    fetch url changed to recheck the error

--- app/lib/getProjects.js
+++ app/lib/getProjects.js
@@ -1,5 +1,7 @@
 export default async function getProjects() {
-  const res = await fetch(`${process.env.HOST_URL}/api/projects`);
+  // const res = await fetch(`${process.env.HOST_URL}/api/projects`);
+  const res = await fetch(`https://ah-akash.vercel.app/api/projects`);
+  console.log(res);
   if (!res.ok) {
     throw new Error('Failed to fetch projects');
   }
